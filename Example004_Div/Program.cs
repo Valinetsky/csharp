c8919d372e5b1c5615350d9f41ad167ed67036b8
Valinetsky <highrat@gmail.com>
2022-11-28 08:57:15 +0300

    Add random numbers in Example003

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -10,3 +10,5 @@ double numberA = 12;
 double numberB = 5;
 // int result = numberA + numberB;
 Console.WriteLine(numberA / numberB);
+
+// new Random().Next(min, max); — случайное число от min до max - 1
