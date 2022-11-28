dc9f97db8a9f51bfe60b53469046075cdde97b4f
Valinetsky <highrat@gmail.com>
2022-11-28 09:14:33 +0300

    Added Example 005 Condition If-Else whitout Else Only If-If

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mary")
+{
+	Console.WriteLine("Nice to meet you, Mary!!!");
+}
+
+if (username.ToLower() != "mary")
+{
+	Console.WriteLine("Hello, " + username);
+}
