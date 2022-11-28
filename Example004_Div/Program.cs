81952bf0fc66b04fc48bf170c0180c5379d0e325
Valinetsky <highrat@gmail.com>
2022-11-28 08:43:21 +0300

    Add Example004 Div Project

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,12 @@
+﻿// Console.WriteLine("Hello!");
+// Console.Write("Input first number ");
+// firstNumber = int.Parse(Console.ReadLine());
+// Console.Write("Input second number ");
+// secondNumber = int.Parse(Console.ReadLine());
+// sum = firstNumber + secondNumber;
+// Console.WriteLine("Sum of two numbers ", sum);
+
+int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
