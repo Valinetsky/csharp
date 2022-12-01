c828be4813a1afbc737c3e6df58dde426bd46197
Valinetsky <highrat@gmail.com>
2022-12-01 09:41:01 +0300

    Added Example010 Method Array

---
+++ Example010_Method_Array/Program.cs
@@ -0,0 +1,17 @@
+﻿// 				 0    1   2  3   4  5   6    7   8
+int[] array = { 14, 325, 37, 4, 15, 6, 47, 998, 19 };
+
+int n = array.Length;
+int find = 47;
+
+int index = 0;
+
+while (index < n)
+{
+	if (array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
